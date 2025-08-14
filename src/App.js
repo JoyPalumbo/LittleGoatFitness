31c70312f60c5e57689a70b628dacd39246fc332
JoyPalumbo <greeniejoy@gmail.com>
2025-08-14 13:07:39 -0400

    Updated routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 
 import './App.css';
 // import { Routes, Route } from "react-router-dom"
+// import { BrowserRouter } from "react-router";
+  import { BrowserRouter, Routes, Route } from 'react-router-dom';
+  import { NavLink, Link } from "react-router";
 import Header from './Header';
 import Home from './Home'
 import About from './About'
 import Contact from './Contact'
+import Nav from './Nav'
 
 function App() {
   return (
 
       <body className='background'> 
           <div className="App">
-
+               <BrowserRouter>
+      <Nav /> {/* Navbar appears on all routes */}
+      <Routes>
+     <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+    {/* <BrowserRouter>
+    </BrowserRouter> */}
+ {/* <NavLink to="/About" >About</NavLink> */}
+    {/* <Router>
+      <Blog />
+      <Route path="about" element={ <About/> } />
+    </Router> */}
           {/* <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="about" element={ <About/> } />
         <Route path="contact" element={ <Contact/> } />
-      </Routes> */}
-      {/* <Header/>
-      <Home /> */}
+      </Routes>  */}
+
+   
       </div>
       </body>
 
--- src/Nav.js
+++ src/Nav.js
@@ -1,21 +1,40 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
 
 import './App.css';
+
 
 const Navbar = () => {
   return (
 
 <nav className="navbar">
+ <div className="navbar-left">
+    <h1 className="headertitle ">Little Goat Fitness</h1>
+
+  </div>
+    <div className="navbar-center"></div>
+      <ul>
+        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+
   {/* <div className="navbar-left">
     <a href="/" className="logo">
       ShopNow
     </a>
   </div> */}
-  <div className="navbar-center">
+
+
+  {/* <div className="navbar-center">
+    
     <ul className="nav-links">
     <li>
-        <a href="/home">Home</a>
-      </li>
+        {/* <a href="/home">Home</a> */}
+      {/* </li>
       <li>
 
       <a href="/about">About Us</a>
@@ -27,7 +46,9 @@ const Navbar = () => {
         <a href="/contact">Contact</a>
       </li>
     </ul>
-  </div>
+  </div> */} 
+
+
   {/* <div className="navbar-right">
     <a href="/cart" className="cart-icon">
       <i className="fas fa-shopping-cart"></i>
